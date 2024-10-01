0325a26fe62163e7e81aaeb9e74851761f8ed2ff
io.jay <164489892+jay-io@users.noreply.github.com>
2024-10-01 21:52:00 +0800

    Create reciept.php

---
+++ act2/reciept.php
@@ -0,0 +1,22 @@
+<?php
+session_start();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Receipt</title>
+</head>
+<body>
+    <h1>=======RECEIPT======</h1>
+    <p>Total Price: <?php echo $_SESSION['totalPrice']; ?></p>
+    <p>Cash: <?php echo $_SESSION['cash']; ?></p>
+    <p>CHANGE: <?php echo $_SESSION['change']; ?></p>
+    <p><?php echo $_SESSION['timestamp']; ?></p>
+    <h1>====================</h1>
+    <form action="index.php" method="get">
+        <input type="submit" value="Next Order">
+    </form>
+</body>
+</html>
